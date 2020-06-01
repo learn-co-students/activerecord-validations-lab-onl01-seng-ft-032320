463e800fe1cfbcbf5e9a72cc49b9a077d7fd2d99
Ryan-McFadden <ryanmcfadden123@gmail.com>
2020-06-01 15:49:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # %w(#{foo} Bar Bar\ with\ space)
+    # => ["\#{foo}", "Bar", "Bar with space"]
+
+    validate :non_clickbait
+
+    def non_clickbait
+        if self.title && !self.title.include?("Won't Believe") && !self.title.include?("Secret") && !self.title.include?("Top [number]") && !self.title.include?("Guess")
+            errors.add(:title, "not clickbait")
+        end
+    end
 end
